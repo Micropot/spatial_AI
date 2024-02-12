a9fb870379983f377dd11f2782dec6feadf03634
Micropot <arthur@lamard.org>
2024-02-12 14:51:14 +0100

    départ

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -10,15 +10,33 @@ import csv
 
 class Lieu:
     def __init__(self):
-
+        # cooredonnes du lieu à visiter
         self.x = None
         self.y = None
+        # nom du lieu
+        self.nom = None
+
+    def distance(self, lieu):
+        return np.sqrt((self.x - lieu.x)**2 + (self.y - lieu.y)**2)
 
 
 class Graph:
     def __init__(self):
         self.largeur = 800
         self.hauteur = 800
+        self.liste_lieux = []
+        self.nb_lieux = 10
+        self.matrice_od = None
+        self.plus_proche_voisins = None
+
+    def calcul_matrice_cout_od(self):
+        self.matrice_od = np.zeros((self.nb_lieux, self.nb_lieux))
+        for i in range(self.nb_lieux):
+            for j in range(self.nb_lieux):
+                self.matrice_od[i, j] = self.liste_lieux[i].distance(self.liste_lieux[j])
+
+    def plus_proche_voisins(self):
+        print("Calcul des plus proches voisins")
 
 
 
