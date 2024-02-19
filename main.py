1189f82993513b3048d69f4ccbab3a1ba37350aa
Micropot <arthur@lamard.org>
2024-02-19 08:08:41 +0100

    premier affichage+TODO

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tsp_graph_init
 
 
 def main():
-    MyGraph = tsp_graph_init.Graph(csv_file="/Users/arthurlamard/Documents/ISEN5/spacial_AI/lieux/graph_20.csv")
+    MyGraph = tsp_graph_init.Graph(csv_file=None)
     MyGraph.charger_graph()
     MyGraph.calcul_matrice_cout_od()
     #MyGraph.plus_proche_voisins(lieu=0, voisins=list(range(MyGraph.nb_lieux)))
